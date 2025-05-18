0e9c0fe4ea23eb417ff8d490a46737d82da7ec14
Gerardo Rapeta Jr <g3r4rdo.r@gmail.com>
2025-05-18 16:16:41 -0700

    building booking page

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -1,10 +1,82 @@
+import {
+  BookingForm,
+  BookingFormContainer,
+  BookingFormWrapper,
+  ContentContainer,
+  ContentWrapper,
+  FormGroup,
+  StyledBookingPage,
+  StyledHeader,
+} from "../styles/BookingStyles";
+import { useState } from "react";
 
 
 export default function Booking() {
+
+  const [formData, setFormData] = useState({
+    requested_date: "",
+    contact_info: "",
+    type: "",
+    additional_info: ""
+  })
+
+  const { requested_date, contact_info, type, additional_info } = formData
+
+  function handleChange(e) {
+    let name = e.target.name
+    let value = e.target.value
+    setFormData({ ...formData, [name]: value })
+  }
+
   return (
-    <div>
-      <h1>Booking</h1>
-      <p>This is the booking page.</p>
-    </div>
+    <StyledBookingPage>
+      <ContentWrapper>
+        <ContentContainer>
+          <StyledHeader>
+            <h1>
+              Booking
+            </h1>
+          </StyledHeader>
+          <BookingFormWrapper>
+            <BookingFormContainer>
+              <BookingForm>
+                <FormGroup>
+                  <label>Requested Date</label>
+                  <input name="requested_date"
+                    value={requested_date}
+                    type="text"
+                    onChange={(e) => handleChange(e)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <label>Contact</label>
+                  <input name="contact_info"
+                    value={contact_info}
+                    type="text"
+                    onChange={(e) => handleChange(e)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <label>Type</label>
+                  <input name="type"
+                    value={type}
+                    type="text"
+                    onChange={(e) => handleChange(e)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <label>Additional info</label>
+                  <input name="additional_info"
+                    value={additional_info}
+                    type="text"
+                    onChange={(e) => handleChange(e)}
+                  />
+                </FormGroup>
+              </BookingForm>
+            </BookingFormContainer>
+          </BookingFormWrapper>
+        </ContentContainer>
+      </ContentWrapper>
+    </StyledBookingPage>
   );
 }
---
+++ src/styles/BookingStyles.js
@@ -0,0 +1,34 @@
+import { style } from 'framer-motion/client';
+import styled from 'styled-components';
+
+export const StyledBookingPage = styled.div`
+
+`;
+
+export const ContentWrapper = styled.div`
+
+`;
+
+export const ContentContainer = styled.div`
+
+`;
+
+export const StyledHeader = styled.div`
+
+`;
+
+export const BookingFormWrapper = styled.div`
+
+`;
+
+export const BookingFormContainer = styled.div`
+
+`;
+
+export const BookingForm = styled.form`
+
+`;
+
+export const FormGroup = styled.div`
+
+`;
